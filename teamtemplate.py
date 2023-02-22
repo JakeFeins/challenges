596c57d473b2263b2e1e2903549e001ee4667bcc
Jake Feinstein <97689799+JakeFeins@users.noreply.github.com>
2023-02-22 17:03:29 -0500

    Add files via upload

---
+++ teamtemplate.py
@@ -0,0 +1,17 @@
+def team_template():
+    # Define the parameters of the pipe
+    L = 10  # length of the pipe
+    d = 0.1  # diameter of the pipe
+    e = 0.001  # roughness of the pipe
+
+    # Define the properties of the fluid
+    rho = 1000  # density of the fluid
+    mu = 1e-3  # viscosity of the fluid
+
+    #####################################
+    # YOUR CODE GOES HERE
+    # Calculate the flow rate through the pipe
+    Q_calc = 0
+    #####################################
+
+    return Q_calc
